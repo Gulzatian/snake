eb7caccc828fbea825edcd180ecae268c34a97ee
Gulzatian <gulzatian@gmail.com>
2016-09-28 19:50:08 +0300

    Added 2 Lists - Symbols and points

--- Game/Snake/Snake/Program.cs
+++ Game/Snake/Snake/Program.cs
@@ -10,11 +10,27 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');
-            p1.Draw();
+            List<char> cList = new List<char>();
+            cList.Add('*');
+            cList.Add('#');
+            cList.Add('@');
+            cList.Add('%');
 
-            Point p2 = new Point(4, 5, '#');
-            p2.Draw();
+            Point p1 = new Point(1, 3, cList[0]);
+            Point p2 = new Point(4, 5, cList[1]);
+            Point p3 = new Point(7, 9, cList[2]);
+            Point p4 = new Point(9, 14, cList[3]);
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+            pList.Add(p3);
+            pList.Add(p4);
+
+            foreach (Point i in pList)
+            {
+                i.Draw();
+            }
 
             Console.ReadLine();
         }
